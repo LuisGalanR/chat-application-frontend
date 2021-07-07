34c7f3fcdbe9d3c6d3a79177128c1add8f557f3b
LuisGalanR <lgruiz.97@gmail.com>
2021-07-07 23:27:34 +0200

    [LGR] - Chat Service
    Building chat.service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { ChatService } from './services/chat/chat.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'chat-application-frontend';
+export class AppComponent implements OnInit {
+
+  public roomId: string;
+  public messageText: string;
+  public messageArray: {user: string, message: string}[] = [];
+
+  public phone: string;
+  public currentUser;
+  public selectedUser;
+
+  public userList = [
+    {
+      id: 1,
+      name: 'The Swag Coder',
+      phone: '9876598765',
+      image: 'assets/user/user-1.png',
+      roomId: {
+        2: 'room-1',
+        3: 'room-2',
+        4: 'room-3'
+      }
+    },
+    {
+      id: 2,
+      name: 'Wade Warren',
+      phone: '9876543210',
+      image: 'assets/user/user-2.png',
+      roomId: {
+        1: 'room-1',
+        3: 'room-4',
+        4: 'room-5'
+      }
+    },
+    {
+      id: 3,
+      name: 'Albert Flores',
+      phone: '9988776655',
+      image: 'assets/user/user-3.png',
+      roomId: {
+        1: 'room-2',
+        2: 'room-4',
+        4: 'room-6'
+      }
+    },
+    {
+      id: 4,
+      name: 'Dianne Russell',
+      phone: '9876556789',
+      image: 'assets/user/user-4.png',
+      roomId: {
+        1: 'room-3',
+        2: 'room-5',
+        3: 'room-6'
+      }
+    }
+  ];
+
+  constructor(private chatService: ChatService) {
+
+  }
+
+  ngOnInit(): void {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,6 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
